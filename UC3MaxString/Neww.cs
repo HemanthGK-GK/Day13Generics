51cb6ddd59f99090875c70b2879ab70e6446690d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 17:49:40 +0530

    Hemanth : Find maximum of 3 Strings

---
+++ UC3MaxString/Neww.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3MaxString
+{
+    class Neww
+    {
+        public void findMaximum(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("The Greatest Number is : " + first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("The Greatest Number is : " + second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("The Greatest Number is : " + third);
+            }
+            else
+                Console.WriteLine("All are equal");
+
+        }
+    }
+}
---
+++ UC3MaxString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC3MaxString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neww obj = new Neww();
+            obj.findMaximum("Maintain", "Silence", "Perfect");
+        }
+    }
+}
