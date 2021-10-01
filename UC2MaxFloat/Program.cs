7a1780fa0a72fbe679209c46144688adf260b849
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 17:46:48 +0530

    Hemanth : Finf maximum of 3 Float number

---
+++ UC2MaxFloat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC2MaxFloat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neww obj = new Neww();
+            obj.findMaximum(5.0f, 4.6f, 9.7f);
+        }
+    }
+}
