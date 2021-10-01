fcdbe281bbfff5d5fb1c17adfa860e331cacabbb
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 17:42:18 +0530

    Hemanth : Find Maximunm of 3 Integer

---
+++ UC1MaxOf3Num/Neww.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1MaxOf3Num
+{
+    public class Neww
+    {
+        public void findMaximum(int first,int second,int third)
+        {
+            if(first.CompareTo(second)>0 &&first.CompareTo(third)> 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("The Greatest Number is : " + first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) > 0 && second.CompareTo(third) >= 0 )
+            {
+                Console.WriteLine("The Greatest Number is : " + second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+                third.CompareTo(first) > 0 && third.CompareTo(second) >= 0 )
+            {
+                Console.WriteLine("The Greatest Number is : " + third);
+            }else
+                Console.WriteLine("All are equal");
+
+        }
+    }
+}
---
+++ UC1MaxOf3Num/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC1MaxOf3Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Neww obj = new Neww();
+            obj.findMaximum(52,86,92);
+        }
+    }
+}
