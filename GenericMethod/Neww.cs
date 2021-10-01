3934c6feea3909baee3c80032467164ca47bc109
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 17:53:25 +0530

    Hemanth : Maximum of 3 variables using Generic Method

---
+++ GenericMethod/Neww.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMethod
+{
+    class Neww 
+    {
+        public T findMaximum <T> (T first,T second,T third) where T : struct, IComparable<T>
+        {
+            
+                
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)                
+            {
+                Console.WriteLine("The Greatest Number is : " + first);
+              return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 )
+            {
+                Console.WriteLine("The Greatest Number is : " + second);
+                 return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 )
+            {
+                Console.WriteLine("The Greatest Number is : " + third);
+                 return third;
+            }
+            return default;
+        }
+    }
+}
---
+++ GenericMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neww ob = new Neww();
+            ob.findMaximum<int>(25, 63, 14);
+            ob.findMaximum<float>(4.2f,5.6f,69.4f);
+            ob.findMaximum<char>('a', 'b', 'c');
+            
+            
+        }
+    }
+}
