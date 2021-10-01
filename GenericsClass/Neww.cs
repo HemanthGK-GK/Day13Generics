f8c0a81496138bcc40ecd65f2ede9006940f626f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-01 18:01:21 +0530

    Hemanth : Maximum  of 3 variable using Generic Class

---
+++ GenericsClass/Neww.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsClass
+{
+   public class Neww<T> where T : IComparable
+    {
+       private T first, second, third;
+        public Neww(T first,T second,T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public void findMaximun()
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("The Greatest is : " + first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("The Greatest  is : " + second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("The Greatest  is : " + third);
+            }
+            else
+                Console.WriteLine("All are equal");
+        }
+    }
+}
---
+++ GenericsClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neww<int> intt = new Neww<int>(25,46,16);
+            intt.findMaximun();
+            Neww<float> chr = new Neww<float>(7.5f,8.0f,2.6f);
+            chr.findMaximun();
+            Neww<string> str = new Neww<string>("not", "the", "problrm");
+            str.findMaximun();
+        }
+    }
+}
