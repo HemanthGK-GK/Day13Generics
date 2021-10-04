673d6823421fb32f5b943c86742fe5c3a1bc0b6b
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-04 14:50:25 +0530

    Hemanth : Max Method for more than 3 Parameter

---
+++ UC4MaxGenerics/Neww.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4MaxGenerics
+{
+    class Neww
+    {
+        public T[] Sort<T>(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T findMaximum<T>(params T[] values ) where T : IComparable<T>
+        {
+            var Svalues = Sort(values);
+            return Svalues[Svalues.Length - 1];
+        }
+    }
+}
---
+++ UC4MaxGenerics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC4MaxGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] intArray = { 12, 56, 95, 74, 49 };
+            Neww intt = new Neww();
+            int i=intt.findMaximum<int>(intArray);
+            Console.WriteLine(i);
+
+            float[] floatArray = { 12.1f, 5.6f, 19.5f, 74.9f, 49.7f };
+            Neww flo = new Neww();
+            float f = flo.findMaximum<float>(floatArray);
+            Console.WriteLine(f);
+
+            string[] strArray = { "My", "First", "Project", "Git", "Apple" };
+            Neww str = new Neww();
+            string s = str.findMaximum<string>(strArray);
+            Console.WriteLine(s);
+
+
+
+        }
+    }
+}
