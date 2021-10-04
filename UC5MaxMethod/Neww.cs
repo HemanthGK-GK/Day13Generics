7cbcb6fcfbcec962a6b332afd1ca48924a96f8c2
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-04 14:54:35 +0530

    Hemanth : Extend Max Method to print Max to std out

---
+++ UC5MaxMethod/Neww.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC5MaxMethod
+{
+    class Neww
+    {
+        public T[] Sort<T>(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T printMax<T>(params T[] values) where T : IComparable<T>
+        {
+            
+            var Svalues = Sort(values);
+            return Svalues[Svalues.Length - 1];
+            
+        }
+        public T testMax<T>(T[] values) where T : IComparable<T>
+        {
+            T max = printMax(values);
+            Console.WriteLine("Maximum value is " + max);
+            return max;
+           
+        }
+
+
+    }
+}
---
+++ UC5MaxMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UC5MaxMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 97, 85, 46, 75, 35 };
+            Neww intt = new Neww();
+            intt.printMax<int>(intArray);
+            intt.testMax(intArray);
+
+            string[] strArray = { "My", "First", "Project", "Git", "Apple" };
+            Neww str = new Neww();
+            string s = str.printMax<string>(strArray);
+            intt.testMax(strArray);
+
+        }
+    }
+}
